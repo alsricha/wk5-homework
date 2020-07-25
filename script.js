6828d462bf398b578cf5e9ecf66a6cdd8b68ef73
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-07-25 16:09:31 -0600

    moment() in js

---
+++ script.js
@@ -0,0 +1,22 @@
+//Loads HTML and CSS first before running JS
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
