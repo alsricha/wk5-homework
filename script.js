6c009a2ce739ecb930544895fd3b5bd841b6a4a0
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-07-25 16:57:54 -0600

    Add event listener to save button

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 //Loads HTML and CSS first before running JS
 $(document).ready(function() {
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
+//Assign event listener to save button
+
+$(".saveBtn").click(function(){
+    console.log(this) // "this" represents .saveBtn
+
+    // ".siblings()" is  a selector that gets the children of each element in the set of matched elements. source: https://api.jquery.com/siblings/
+    var text = $(this).siblings(".description").val();
+
+    // ".parent()" is a selector that gets the parent of each element in the current set of matched elements. source: https://api.jquery.com/parent/#parent-selector
+
+    var time = $(this).parent().attr("id");
+
+    // Set Items to Local Storage
+    localStorage.setItem(time, text);
+
+})
 
 
 
